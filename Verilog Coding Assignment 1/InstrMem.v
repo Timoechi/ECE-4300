a8abece4b5941878f18332a3b2a35e9beeabfa0d
Nediaj231 <jaidenphung1@gmail.com>
2026-03-14 22:36:14 -0700

    Add files via upload

--- Verilog Coding Assignment 1/InstrMem.v
+++ Verilog Coding Assignment 1/InstrMem.v
@@ -1,10 +1,25 @@
 
 module InstrMem(
-    output [3:0] Q,
-    input CLR, clk, L_in, R_in,
-    input [1:0] S,
-    input [3:0] D
+    output reg [31:0] Instr,
+    input clk, memread,
+    input [8:0] Addr
 );
-    
+reg [31:0] mem [2^32:0];
+reg [31:0] temp;
+
+
+integer i;
+initial begin
+    for (i = 0; i < (2^32); i = i + 1) begin
+    mem[i] = 32'b0;
+    end
+end
+
+always@(Instr, memread, posedge clk) begin
+    if (memread) begin
+        temp = Addr>>2;
+        Instr = mem[temp];
+    end
+end
 
 endmodule
