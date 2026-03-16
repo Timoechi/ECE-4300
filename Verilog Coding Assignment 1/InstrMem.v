64e28d715baef227b3c593f0446a41f488584a09
KevboPrz <kevbo.prz@gmail.com>
2026-03-15 19:26:58 -0700

    Refactor InstrMem module and update memory initialization

--- Verilog Coding Assignment 1/InstrMem.v
+++ Verilog Coding Assignment 1/InstrMem.v
@@ -1,25 +1,31 @@
-
-module InstrMem(
-    output reg [31:0] Instr,
-    input clk, memread,
-    input [8:0] Addr
+module instrMem(
+    input wire clk,
+    input wire rst,
+    input wire [31:0] addr,
+    output reg [31:0] data
 );
-reg [31:0] mem [2^32:0];
-reg [31:0] temp;
 
+reg [31:0] memory [0:1023];
 
-integer i;
 initial begin
-    for (i = 0; i < (2^32); i = i + 1) begin
-    mem[i] = 32'b0;
-    end
+    memory[0] = 32'hA00000AA;
+    memory[1] = 32'h10000011;
+    memory[2] = 32'h20000022;
+    memory[3] = 32'h30000033;
+    memory[4] = 32'h40000044;
+    memory[5] = 32'h50000055;
+    memory[6] = 32'h60000066;
+    memory[7] = 32'h70000077;
+    memory[8] = 32'h80000088;
+    memory[9] = 32'h90000099;
 end
 
-always@(Instr, memread, posedge clk) begin
-    if (memread) begin
-        temp = Addr>>2;
-        Instr = mem[temp];
-    end
+always @(*)
+begin
+    if (rst)
+        data <= 0;
+    else
+        data <= memory[addr[31:2]]; // word aligned
 end
 
 endmodule
